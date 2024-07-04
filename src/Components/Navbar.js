906c64da784cb8e37f3d81897126ac13f2629d62
Atul Kumar <tyagiatul23799@gmail.com>
2024-07-04 09:54:28 +0530

    changes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,12 @@
 import React from "react";
-import logo from "./logo.png";
+import almabetterlogo from "./almalogo.png";
 
 const Navbar = () => {
   return (
     <div>
       <nav className="px-10 py-5 bg-white shadow-md">
         <div className="w-48">
-          <img src={logo} alt="logo" className="inline-block" />
+          <img src={almabetterlogo} alt="logo" className="inline-block" />
         </div>
       </nav>
     </div>
